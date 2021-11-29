5aab84bc763d2414105e6830966d262d50cb0443
gs432 <glennstraughn@gmail.com>
2021-11-29 04:30:53 +0000

    various

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,6 +7,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     @Override
@@ -19,6 +22,10 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
+        ResourceBundle rb = ResourceBundle.getBundle("Main/Nat", Locale.getDefault());
+
+        if (Locale.getDefault().getLanguage().equals("en") || Locale.getDefault().getLanguage().equals("fr"))
 
         JDBC.openConnection();
         //DBCountries.checkDateConversion();
